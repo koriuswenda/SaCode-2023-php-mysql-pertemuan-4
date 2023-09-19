020e88e57579169350d1ea14611276402ff5a011
korius kussa wenda <koriuswenda@gmail.com>
2023-09-19 16:35:55 +0700

    Sacode-php-pertemuan-4

---
+++ string.php
@@ -0,0 +1,22 @@
+<?php 
+// TIPE DATANG STRING
+$title = "KELAS PHP SACODE";
+$nama_depan = 'korius';
+$nama_belakang = 'wenda';
+$alamat = 'waena';
+$hoby = "singing";
+var_dump($title)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TIPE DATA STRING</title>
+</head>
+<body>
+    <h2> <?= $title ?>  </h2>
+    <p> Nama saya <?= $nama_depan  .' '. $nama_belakang?>, saya tinggal di  </p>
+</body>
+</html>
