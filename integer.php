f278c5d3a38de3bff99a64fdf57525f6a7b379e4
korius kussa wenda <koriuswenda@gmail.com>
2023-09-19 16:59:51 +0700

    SaCode-2023php-mysql-pertemuan-4

---
+++ integer.php
@@ -0,0 +1,43 @@
+<?php
+$angka_satu =50;
+$angka_dua =10;
+$angka_tiga =77;
+
+
+// tambah
+$tambah = $angka_satu + $angka_dua + $angka_tiga;
+
+// kurang
+$kurang = $angka_satu - $angka_dua;
+
+//kali
+$kali = $angka_satu * $angka_tiga;
+
+//bagi
+
+$bagi = $angka_satu / $angka_dua;
+
+$perhitungan = 1+2 *3 ;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TIPE DATA ENTEGER</title>
+</head>
+<body>
+    <p>penambahan</p>
+    <p><?=$tambah?></p>
+    <p>pengurangan</p>
+    <p><?=$kurang?></p>
+    <p>perkalian</p>
+    <p><?=$kali?></p>
+    <p>pembagian</p>
+    <p><?=$bagi?></p>
+    
+
+    
+</body>
+</html>
